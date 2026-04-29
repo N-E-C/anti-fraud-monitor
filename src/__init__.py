897b2b43d987efee63e48a7c8ce43f27c9448d42
N-E-C <67261086+N-E-C@users.noreply.github.com>
2026-04-29 13:49:02 +0800

    chore: remove internal company references

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 """
 客户自媒体反诈微舆情监测工具
-China Mobile Ningxia - Anti-Fraud Sentiment Monitor
+Anti-Fraud Public Opinion Monitoring System
 """
 
 __version__ = "1.0.0"
-__author__ = "China Mobile Ningxia Anti-Fraud Team"
+__author__ = "Anti-Fraud Monitor Team"
